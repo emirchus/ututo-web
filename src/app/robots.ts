066dc773d3b4a1f631aa62d31cdb70e41a476114
Emir Ali <emirhassan.ali02@gmail.com>
2024-07-29 16:10:29 -0300

    feat: añado sitemap y robots.txt

---
+++ src/app/robots.ts
@@ -0,0 +1,19 @@
+import type { MetadataRoute } from 'next';
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: [
+      {
+        userAgent: 'Googlebot',
+        allow: ['/'],
+        disallow: '/private/',
+      },
+      {
+        userAgent: ['Applebot', 'Bingbot'],
+        disallow: ['/'],
+      },
+    ],
+    sitemap: 'https://ututo.emirchus.ar/sitemap.xml',
+    host: 'https://ututo.emirchus.ar',
+  };
+}
